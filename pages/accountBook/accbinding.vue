39c12768b0e6500a0d5c295014714f6ce217634a
saijianga <jiangsai0218@163.com>
2023-05-16 16:56:15 +0800

    添加刷新组件

--- pages/accountBook/accbinding.vue
+++ pages/accountBook/accbinding.vue
@@ -1,46 +1,50 @@
 <template>
 	<view>
-		<view class="item-area">
-			<view class="title-area">
-				<text>支付宝付款专用账户</text>
-			</view>
-			<view class="binding-area" @click="bindingPayAction">
-				<text>+</text>
-				<text>添加支付宝付款专用账户</text>
-			</view>
-		</view>
+		<scroll-view class="scr-area">
+			
 		
-		<view class="item-area">
-			<view class="title-area">
-				<text>支付宝收款账户</text>
-			</view>
-			<view class="binding-area" @click="bindingReachAction">
-				<text>+</text>
-				<text>添加支付宝收款账户</text>
-			</view>
-		</view>
-		
-		<view class="have-item-area">
-			<view class="have-t-area">
-				<text>支付宝付款专用账户zhifubao@ceshi.com</text>
-				<text class="tag-text">签约账户</text>
-			</view>
-			<view class="ali-item-area"  v-for="(item,index) in objList" :key="index">
-				<view class="ali-type-area">
-					<text>专用账户{{item}}:1234567</text>
-					<text>{铁路运费贷平台}</text>
+			<view class="item-area">
+				<view class="title-area">
+					<text>支付宝付款专用账户</text>
 				</view>
-				<view class="ali-money-area">
-					<text class="money-t-css">账户余额</text>
-					<text>0.00</text>
-				</view>
-				<view class="btns-areas">
-					<text>资金拨入</text>
-					<text style="margin-left: 15px;">交易明细</text>
+				<view class="binding-area" @click="bindingPayAction">
+					<text>+</text>
+					<text>添加支付宝付款专用账户</text>
 				</view>
 			</view>
-		</view>
-		
+			
+			<view class="item-area">
+				<view class="title-area">
+					<text>支付宝收款账户</text>
+				</view>
+				<view class="binding-area" @click="bindingReachAction">
+					<text>+</text>
+					<text>添加支付宝收款账户</text>
+				</view>
+			</view>
+			
+			<view class="have-item-area">
+				<view class="have-t-area">
+					<text>支付宝付款专用账户zhifubao@ceshi.com</text>
+					<text class="tag-text">签约账户</text>
+				</view>
+				<view class="ali-item-area"  v-for="(item,index) in objList" :key="index">
+					<view class="ali-type-area">
+						<text>专用账户{{item}}:1234567</text>
+						<text>{铁路运费贷平台}</text>
+					</view>
+					<view class="ali-money-area">
+						<text class="money-t-css">账户余额</text>
+						<text>0.00</text>
+					</view>
+					<view class="btns-areas">
+						<text>资金拨入</text>
+						<text style="margin-left: 15px;">交易明细</text>
+					</view>
+				</view>
+			</view>
+			
+		</scroll-view>
 	</view>
 </template>
 
@@ -65,6 +69,13 @@
 </script>
 
 <style lang="scss" scoped>
+
+	.scr-area{
+		width: 100%;
+		height: 100%;
+		background-color: transparent;
+	}
+
 
 	.item-area{
 		background-color: #fff;
