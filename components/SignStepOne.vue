9ed704916c84ab5e9a0517c5579a3f42bba688fe
saijianga <jiangsai0218@163.com>
2023-05-16 11:38:26 +0800

    更新

---
+++ components/SignStepOne.vue
@@ -0,0 +1,93 @@
+<template>
+	<view>
+		<view class="line-area">
+			<text class="tip-text">i</text>
+			<text>绑定企业支付宝,需验证企业支付宝法定代表人/负责人信息。</text>
+		</view>
+		<view class="bottom-area">
+			<text>法定代表人/负责人</text>
+			<view class="item-area">
+				<view class="left-t-css">
+					<text>*</text>
+					<text>姓名:</text>
+				</view>
+				<input placeholder="请输入"/>
+			</view>
+			<view class="item-area">
+				<view class="left-t-css">
+					<text>*</text>
+					<text>证件号码:</text>
+				</view>
+				<input placeholder="请输入"/>
+			</view>
+		</view>
+		
+		<button class="btns-css">下一步</button>
+		
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"SignStepOne",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+.line-area{
+	background-color: #e6f7ff;
+	border: 1px solid #91d5ff;
+	width: 100%;
+	height: 40px;
+	
+	display: flex;
+	align-items: center;
+	
+	.tip-text{
+		width: 20px;
+		height: 20px;
+		border-radius: 10px;
+		text-align: center;
+		background-color: #0076ff;
+		color: #fff;
+		margin-right: 5px;
+	}
+	
+}
+
+.bottom-area{
+	margin-top: 10px;
+	background-color: #fff;
+	padding: 15px 10px 20px 20px;
+	
+	.item-area{
+		margin-top: 20px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		
+		.left-t-css{
+			width: 120px;
+		}
+		
+	}
+	
+}
+
+.btns-css{
+	background-color: #0076ff;
+	color: #fff;
+	width: 120px;
+	height: 45px;
+	margin-top: 30px;
+}
+
+</style>
--- pages/accountBook/accsign.vue
+++ pages/accountBook/accsign.vue
@@ -1,11 +1,15 @@
 <template>
 	<view>
 		<scroll-item :title-arr="titleArr" :setpnumber="setpnumber"></scroll-item>
+		<view>
+			<sign-one v-if="setpnumber == 1"></sign-one>
+		</view>
 	</view>
 </template>
 
 <script>
 	import ScrollItem from '../../components/ScroItem.vue'
+	import SignOne from '../../components/SignStepOne.vue'
 	export default {
 		data() {
 			return {
@@ -14,7 +18,8 @@
 			};
 		},
 		components:{
-			ScrollItem
+			ScrollItem,
+			SignOne
 		}
 		
 		
